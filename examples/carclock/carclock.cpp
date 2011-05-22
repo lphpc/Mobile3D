b65c656c214248926b6acf639f1ea79b20c304d5
Liu Peng <lphpc@yahoo.com>
2011-05-22 13:20:33 +0800

    add mac support

--- examples/carclock/carclock.cpp
+++ examples/carclock/carclock.cpp
@@ -1,8 +1,19 @@
 
 #include <sys/time.h>
+
+#ifdef MAC
+
+#include <OpenGL/gl.h>
+#include <OpenGL/glu.h>
+#include <GLUT/glut.h>
+
+#else
+
 #include <GL/gl.h>
 #include <GL/glu.h>
 #include <GL/glut.h>
+
+#endif 
 
 #include <mobile3d/m3d.h>
 #include <mobile3d/world.h>
